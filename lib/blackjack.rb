1ce85157f43140a21f9c8f13aec25be6eef8db4f
Charlie Chung <charlieschung@gmail.com>
2019-09-17 01:39:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,9 +29,16 @@ def initial_round
   return card_total
 end
 
-def hit?
+def hit?(card_total)
   prompt_user
-  get_user_input
+  input=get_user_input
+  if input == 's'
+    card_total
+  elsif input == 'h'
+    card_total+=deal_card
+  else
+    invalid_command
+    hit?(card_total)
 end
 
 def invalid_command
