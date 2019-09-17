f84e45b70e147e0c0158a42e72247586724d3c84
Charlie Chung <charlieschung@gmail.com>
2019-09-17 01:52:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,9 +53,9 @@ end
 def runner
   welcome
   card_total=initial_round
-  #until card_total > 21
-  #  hit?(card_total)
-  #end
-  #end_game
+  card_total=hit?(card_total)
+  display_card_total(card_total)
+  hit?(card_total)
+  end_game
 end
     
